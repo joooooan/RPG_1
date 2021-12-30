7abf7c7b96a41a1f5132bc21466fea68349c3fb2
joooooan <kwonjuan5872@gmail.com>
2021-12-30 14:02:33 +0900

    script

---
+++ CameraRotate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+
+    private float _rotateSpeedX;
+    private float _rotateSpeedY;
+    private float _limitX;
+    private float _eulerAngleX;
+    private float _eulerAngleY;
+
+    private void Awake()
+    {
+        _rotateSpeedX = 3;
+        _rotateSpeedY = 5;
+        _limitX = 80;
+        _eulerAngleX = 0 ;
+        _eulerAngleY = 0;
+    }
+
+    public void RatateTo(float mousex,float mousey)
+    {
+        _eulerAngleY += mousex * _rotateSpeedX;
+
+        _eulerAngleX -= mousey * _rotateSpeedY;
+
+        _eulerAngleX = Mathf.Clamp(_eulerAngleX, -_limitX, _limitX);
+
+        this.transform.rotation = Quaternion.Euler(_eulerAngleX, _eulerAngleY, 0);
+    }
+
+    
+}
---
+++ Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    public float _playerSpeed = 5;
+
+    private float _gravity;
+    private Vector3 _moveDirection;
+
+    private CharacterController _characterController;
+
+    private void Awake()
+    {
+        _characterController = this.GetComponentInChildren<CharacterController>();
+
+        _gravity = -9.8f;
+        _moveDirection = Vector3.zero;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Gravity();
+
+        _characterController.Move(_moveDirection* _playerSpeed* Time.deltaTime);
+    }
+
+    public void MoveTo(Vector3 direction)
+    {
+        Vector3 movdDis = Camera.main.transform.rotation * direction;
+        _moveDirection = new Vector3(movdDis.x, _moveDirection.y, movdDis.z);
+        _moveDirection.Normalize();
+    }
+
+    public void Gravity()
+    {
+        if(_characterController.isGrounded == false)
+        {
+            _moveDirection.y += _gravity * Time.deltaTime;
+        }
+    }
+}
