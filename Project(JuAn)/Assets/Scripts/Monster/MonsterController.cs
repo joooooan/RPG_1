0c7ce5016a2b42af694ababdab886f1145d9ba66
joooooan <kwonjuan5872@gmail.com>
2021-12-31 14:54:20 +0900

    2021.12.31

--- Project(JuAn)/Assets/Scripts/Monster/MonsterController.cs
+++ Project(JuAn)/Assets/Scripts/Monster/MonsterController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class MonsterController : MonoBehaviour
 {
@@ -17,42 +18,119 @@ public class MonsterController : MonoBehaviour
 
     State _monsterState;
 
+    Stat _stat;
+    Ray _ray;
+
+    private float _currTime;
+    private float _limitTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        _monsterState = State.Idle;
+        _monsterState = State.Search;
+        _stat = this.GetComponent<Stat>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_stat == null) return; // 모든 몬스터는 Stat 클래스를 가지고 있어야 한다.
+
         MonsterState();
+
     }
 
     void MonsterState()
     {
-        switch(_monsterState)
+        if(_stat.CurrHp <= 0) _monsterState = State.Dead;
+
+        switch (_monsterState)
         {
             case State.Idle:
+                Idle();
                 break;
 
             case State.Search:
+                Search();
                 break;
 
             case State.Finding:
+                Finding();
                 break;
 
             case State.Move:
+                Move();
                 break;
 
             case State.Attack:
+                Attack();
                 break;
 
             case State.Return:
+                Return();
                 break;
 
             case State.Dead:
+                Dead();
                 break;
         }
     }
+
+    private void Idle()
+    {
+        if (_stat.Hp > _stat.CurrHp)
+        {
+            _currTime = 0;
+            _monsterState = State.Search;
+        }
+    }
+
+    private void Search()
+    {
+        _currTime += Time.deltaTime;
+
+        if(_currTime > _limitTime) _monsterState = State.Idle;
+
+        _ray = new Ray(this.transform.position, this.transform.forward);
+        Debug.DrawRay(_ray.origin, _ray.direction * 50.0f, Color.red);
+
+        RaycastHit hit;
+        if (Physics.Raycast(_ray, out hit, 50.0f))
+        {
+
+            if(hit.transform.tag == "Player")
+            {
+                _monsterState = State.Finding;
+            }
+
+            Debug.Log(hit.transform.name);
+        }
+
+    }
+
+    private void Finding()
+    {
+        //무기 상태에 따른 공격 판단.
+    }
+
+    private void Move()
+    {
+        //움직이는 중
+    }
+
+    private void Attack()
+    {
+
+    }
+
+    private void Return()
+    {
+
+    }
+
+    private void Dead()
+    {
+
+    }
+
 }
---
+++ Project(JuAn)/Assets/Scripts/Stat/Stat.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat : MonoBehaviour
+{
+    [SerializeField]
+    private int _hp;
+    public int Hp { get { return _hp; } set { _hp = value; } }
+
+    private int _currhp;
+    public int CurrHp { get { return _currhp; } set { _currhp = value; } }
+
+    [SerializeField]
+    private int _mp;
+    public int Mp { get { return _mp; } set { _mp = value; } }
+
+    private int _currmp;
+    public int CurrMp { get { return _currmp; } set { _currmp = value; } }
+
+    [SerializeField]
+    private int _atk;
+
+    [SerializeField]
+    private int _def;
+
+    [SerializeField]
+    private int _exp;
+
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _currhp = _hp;
+        _currmp = _mp;
+    }
+
+}
